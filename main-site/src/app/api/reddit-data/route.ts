3cbd3f9a83555804a099416838f6aff700b63bff
rizzz6 <rizvialam2005@gmail.com>
2025-08-18 21:08:28 +0530

    smol fixes

--- main-site/src/app/api/reddit-data/route.ts
+++ main-site/src/app/api/reddit-data/route.ts
@@ -2,7 +2,11 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch('https://www.reddit.com/r/wbjee/about.json');
+    const response = await fetch('https://www.reddit.com/r/wbjee/about.json', {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+      },
+    });
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
