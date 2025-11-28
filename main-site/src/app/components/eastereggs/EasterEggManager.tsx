5a6af8642118714579e2a35a1816b694bb8798d3
rizzz6 <rizvialam2005@gmail.com>
2025-11-29 00:08:00 +0530

    easter egg, footer updated, footer unused imports removed

---
+++ main-site/src/app/components/eastereggs/EasterEggManager.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+// Import all your specific eggs here
+import PrintingHack from './PrintingHack';
+// import MatrixHack from './MatrixHack'; // Future use
+
+export default function EasterEggManager() {
+    return (
+        <>
+            {/* List all your active Easter Eggs here */}
+            <PrintingHack />
+            {/* <MatrixHack /> */}
+        </>
+    );
+}
