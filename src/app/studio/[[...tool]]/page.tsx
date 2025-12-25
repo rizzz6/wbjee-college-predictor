f72d7df0ffe851f797c0742c6b33cea2c7c77196
rizzz6 <rizvialam2005@gmail.com>
2025-12-25 17:25:21 +0530

    sanity page build fix

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -6,9 +6,10 @@
  * You can learn more about the next-sanity package here:
  * https://github.com/sanity-io/next-sanity
  */
-
+'use client'
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
+import { client } from '@/sanity/client'
 
 export const dynamic = 'force-static'
 
